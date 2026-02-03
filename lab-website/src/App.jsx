d4b9ffdb3452e2e91912aa6094cf84a989fbd005
Naveen Kamath <nk2017@kamaths.com>
2026-02-03 16:03:23 -0500

    fixed merge conflict

--- lab-website/src/App.jsx
+++ lab-website/src/App.jsx
@@ -2,15 +2,10 @@ import { Routes, Route, useLocation } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import HomePage from "./components/HomePage/HomePage";
 import TempComponent from "./components/PublicationPage/TempComponents";
-<<<<<<< HEAD
-import PublicationsPage from "./components/PublicationPage/PublicationPage";
-import PeoplesPage from "./components/PeoplesPage/PeoplesPage";
-import EventsPage from "./components/EventsPage/EventsPage";
-=======
 import PublicationsLayout from "./components/PublicationPage/PublicationsLayout";
 import PublicationsPage from "./components/PublicationPage/PublicationPage";
 import PublicationDetail from "./components/PublicationPage/PublicationDetail";
->>>>>>> 06dc432693121255f8debcd7ee5af36dcdec6867
+
 import "./App.css";
 
 function App() {
@@ -24,12 +19,7 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/research" element={<TempComponent />} />
-<<<<<<< HEAD
-          <Route path="/people" element={<PeoplesPage />} />
-          <Route path="/publications" element={<PublicationsPage />} />
-          <Route path="/events" element={<EventsPage />} />
 
-=======
           <Route path="/people" element={<TempComponent />} />
 
           {/* Publications with subpages */}
@@ -37,7 +27,6 @@ function App() {
             <Route index element={<PublicationsPage />} />
             <Route path=":id" element={<PublicationDetail />} />
           </Route>
->>>>>>> 06dc432693121255f8debcd7ee5af36dcdec6867
         </Routes>
       </div>
     </>
